906bb0cab80b4e9b0758b97e98bff458ab0601ed
Ashutosh Aman <ashutoshsingh161216@gmail.com>
2021-12-12 18:40:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// ex10: list grocery items to buy
+
+/**
+ * prepare a grocerylist with atleat 5 item
+ * print the first item on the list
+ * print the third item on the list
+ * print the last itrm on the list
+ */
+
+var groceryList = ["dudh","bread", "sabji","daba","phool"];
+
+console.log(groceryList[0]);
+
+console.log(groceryList[2]);
+
+console.log(groceryList[groceryList.length - 1]);
+
+var howLongIsThisArray = groceryList.length;
+
+console.log(howLongIsThisArray);
